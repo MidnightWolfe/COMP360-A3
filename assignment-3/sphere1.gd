46ba2568bf5ecb101e92407314c183619f918c5e
Kaiya Wangler <lilah1305@gmail.com>
2024-10-29 15:35:29 -0700

    Added just some simple spheres and changed the process methods

---
+++ assignment-3/sphere1.gd
@@ -0,0 +1,15 @@
+extends MeshInstance3D
+
+var scalar1 = 0
+var vector1 = 1 #Just placeholders
+
+var force1 = scalar1 * vector1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	pass
